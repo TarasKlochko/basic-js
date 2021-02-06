45a58c8f2deae4f55886ef56f9434deaf1b22c89
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-02-06 17:36:13 +0200

    feat: implement function dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,10 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  return Number.isInteger(sampleActivity) && typeof sampleActivity === 'string'
+    ? Math.log(MODERN_ACTIVITY / sampleActivity) / Math.LN2 / HALF_LIFE_PERIOD
+    : false;
 };
